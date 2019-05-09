192522e74715f1e2e20edab6e582c410d0e34a39
wb-yhb513235 <wb-yhb513235@alibaba-inc.com>
2019-05-09 15:27:12 +0800

    集成ES搜索

---
+++ shopping-elasticsearch-api/src/main/java/com/yhb/elasticsearch/ElasticsearchApplication.java
@@ -0,0 +1,14 @@
+package com.yhb.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class ElasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchApplication.class, args);
+    }
+
+}
